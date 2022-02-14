c673051da91d0297f72255f6377d4a1720ae6fc5
rizal57 <fahrurrzl5798@gmail.com>
2022-02-14 08:12:24 +0700

    menambahkan card buku yang dipinjam di halaman admin

--- admin/page/home.php
+++ admin/page/home.php
@@ -17,6 +17,10 @@ $jumlah_penerbit = count($jumlah_penerbit);
 $jumlah_peminjam = query("SELECT * FROM peminjam");
 $jumlah_peminjam = count($jumlah_peminjam);
 
+// jumlah buku yang dipinjam
+$jumlah_dipinjam = query("SELECT * FROM detail_peminjam");
+$jumlah_dipinjam = count($jumlah_dipinjam);
+
 
 ?>
 
@@ -94,6 +98,27 @@ $jumlah_peminjam = count($jumlah_peminjam);
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-user fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Buku ynag dipinjam</div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumlah_dipinjam; ?></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-book fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
